7d79ee0bcd66628ba63d6f3cd88c27508f88614a
Kayvan Shah <kayvan.shah@usc.edu>
2024-02-08 23:29:35 -0800

    bug fixes

--- assignment-1/solutions/run.sh
+++ assignment-1/solutions/run.sh
@@ -1,3 +1,5 @@
-spark-submit --executor-memory 4G --driver-memory 4G \
-    task1.py ../resource/asnlib/publicdata/test_review.json python_task1.json \
-    --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties
+# Task 1
+spark-submit --executor-memory 4G --driver-memory 4G task1.py ../resource/asnlib/publicdata/test_review.json python_task1.json --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties
+
+# Task 2
+spark-submit --executor-memory 4G --driver-memory 4G task2.py ../resource/asnlib/publicdata/review.json python_task2.json 2 --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=log4j.properties
