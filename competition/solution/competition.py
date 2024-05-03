24b6758b8f6be496422f435f35adec594f90bbc5
Kayvan Shah <kayvan.shah@usc.edu>
2024-05-03 01:27:54 -0700

    added scripts

---
+++ competition/solution/competition.py
@@ -0,0 +1,386 @@
+import csv
+import json
+import os
+import sys
+import time
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+from pyspark import SparkConf, SparkContext
+from xgboost import XGBRegressor
+
+
+class DataReader:
+    def __init__(self, sc: SparkContext, folder_path: str):
+        self.sc = sc
+        self.folder_path = folder_path
+
+    def get_path(self, filename: str):
+        return os.path.join(self.folder_path, filename)
+
+    def read_csv_spark(self, path: str):
+        path = self.get_path(path)
+        rdd = self.sc.textFile(path)
+        header = rdd.first()
+        rdd = rdd.filter(lambda row: row != header).map(lambda row: row.split(","))
+        return rdd, header.split()
+
+    def read_json_spark(self, path: str):
+        path = self.get_path(path)
+        return self.sc.textFile(path).map(lambda row: json.loads(row))
+
+
+class BusinessData:
+    keys_to_delete = [
+        "name",
+        "neighborhood",
+        "address",
+        "attributes",
+        "categories",
+        "hours",
+        "postal_code",
+        "city",
+        "state",
+    ]
+
+    @staticmethod
+    def parse_row(row: dict):
+        row["num_attrs"] = len(row["attributes"]) if row["attributes"] is not None else 0
+        row["num_categories"] = len(row["categories"].split(",")) if row["categories"] is not None else 0
+        # row["latitude"] = float(row["latitude"])
+        # row["longitude"] = float(row["longitude"])
+        row["stars"] = float(row["stars"])
+
+        # Delete keys
+        # row = {k: v for k, v in row.items() if k not in BusinessData.keys_to_delete}
+        return row
+
+    @staticmethod
+    def generate_mapping(bus_rdd):
+        # Extract unique values for state and city
+        state_to_index = bus_rdd.map(lambda row: row["state"]).distinct().zipWithIndex().collectAsMap()
+        city_to_index = bus_rdd.map(lambda row: row["city"]).distinct().zipWithIndex().collectAsMap()
+
+        return state_to_index, city_to_index
+
+    def process(bus_rdd):
+        bus_rdd = bus_rdd.map(lambda row: BusinessData.parse_row(row)).map(
+            lambda row: (
+                row["business_id"],
+                (
+                    row["stars"],
+                    row["review_count"],
+                    row["is_open"],
+                    row["num_attrs"],
+                    row["num_categories"],
+                    # row["latitude"],
+                    # row["longitude"],
+                ),
+            )
+        )
+        return bus_rdd
+
+
+class UserData:
+    keys_to_delete = [
+        "name",
+        "friends",
+        "elite",
+        "yelping_since",
+        "compliment_hot",
+        "compliment_more",
+        "compliment_profile",
+        "compliment_cute",
+        "compliment_list",
+        "compliment_note",
+        "compliment_plain",
+        "compliment_cool",
+        "compliment_funny",
+        "compliment_writer",
+        "compliment_photos",
+    ]
+    compliment_keys = [
+        "compliment_hot",
+        "compliment_more",
+        "compliment_profile",
+        "compliment_cute",
+        "compliment_list",
+        "compliment_note",
+        "compliment_plain",
+        "compliment_cool",
+        "compliment_funny",
+        "compliment_writer",
+        "compliment_photos",
+    ]
+    lc = len(compliment_keys)
+
+    @staticmethod
+    def parse_row(row: dict):
+        row["num_elite"] = len(row["elite"].split(",")) if row["elite"] != "None" else 0
+        row["num_friends"] = len(row["friends"].split(",")) if row["friends"] != "None" else 0
+        row["avg_compliment"] = sum(row[key] for key in UserData.compliment_keys) / UserData.lc
+
+        yelping_since = datetime.strptime(row["yelping_since"], "%Y-%m-%d")
+        membership_years = datetime.now() - yelping_since
+        row["membership_years"] = membership_years.days / 365.25
+
+        row["average_stars"] = float(row["average_stars"])
+
+        # Delete keys
+        # row = {k: v for k, v in row.items() if k not in UserData.keys_to_delete}
+        return row
+
+    def process(user_rdd):
+        user_rdd = user_rdd.map(lambda row: UserData.parse_row(row)).map(
+            lambda row: (
+                row["user_id"],
+                (
+                    row["review_count"],
+                    row["useful"],
+                    row["funny"],
+                    row["cool"],
+                    row["fans"],
+                    row["average_stars"],
+                    row["num_elite"],
+                    row["num_friends"],
+                    row["avg_compliment"],
+                    row["membership_years"],
+                ),
+            )
+        )
+        return user_rdd
+
+
+class ReviewData:
+    keys_to_delete = ["review_id", "date", "text"]
+
+    @staticmethod
+    def parse_row(row):
+
+        # Delete keys
+        row = {k: v for k, v in row.items() if k not in ReviewData.keys_to_delete}
+        return row
+
+    def process(rdd):
+        rdd = (
+            rdd
+            # .map(lambda row: ReviewData.parse_row(row))
+            .map(
+                lambda row: (
+                    (row["user_id"], row["business_id"]),
+                    (row["stars"], row["useful"], row["funny"], row["cool"], 1),
+                )
+            )
+            .reduceByKey(
+                lambda x, y: (x[0] + y[0], x[1] + y[1], x[2] + y[2], x[3] + y[3], x[4] + y[4])  # Sum values and counts
+            )
+            .mapValues(
+                lambda values: (
+                    values[0] / values[4],
+                    values[1] / values[4],
+                    values[2] / values[4],
+                    values[3] / values[4],
+                )  # Calculate averages
+            )
+        )
+        return rdd
+
+
+class TipData:
+    keys_to_delete = ["date", "text"]
+
+    @staticmethod
+    def parse_row(row):
+        # Delete keys
+        row = {k: v for k, v in row.items() if k not in TipData.keys_to_delete}
+        return row
+
+    def process(rdd):
+        rdd = (
+            rdd
+            # .map(lambda row: TipData.parse_row(row))
+            # Calculate sum of likes and review count by user
+            .map(lambda row: ((row["user_id"], row["business_id"]), (row["likes"], 1))).reduceByKey(
+                lambda x, y: (x[0] + y[0], x[1] + y[1])
+            )
+        )
+        return rdd
+
+
+class PhotoData:
+    keys_to_delete = ["photo_id", "caption"]
+    possible_labels = ["drink", "food", "inside", "menu", "outside"]
+
+    @staticmethod
+    def parse_row(row):
+        # Delete keys
+        row = {k: v for k, v in row.items() if k not in PhotoData.keys_to_delete}
+        return row
+
+    def process(rdd):
+        rdd = (
+            rdd
+            # .map(lambda row: PhotoData.parse_row(row))
+            .map(lambda row: ((row["business_id"]), ([row["label"]], 1)))
+            .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+            .mapValues(
+                lambda values: (
+                    # {label: values[0].count(label) for label in PhotoData.possible_labels},
+                    len(set(values[0])),
+                    values[1],
+                )
+            )
+        )
+        return rdd
+
+
+def create_dataset(row, usr_dict, bus_dict, review_dict, tip_dict, img_dict):
+    if len(row) == 3:
+        usr, bus, rating = row
+    else:
+        usr, bus = row
+        rating = None
+
+    r_avg_stars, useful, funny, cool = review_dict.get((usr, bus), (None, 0, 0, 0))
+    (
+        usr_review_count,
+        usr_useful,
+        usr_funny,
+        usr_cool,
+        usr_fans,
+        usr_avg_stars,
+        num_elite,
+        num_friends,
+        usr_avg_comp,
+        membership_years,
+    ) = usr_dict.get(usr, (0, None, None, None, 0, 3.5, 0, 0, 0, None))
+    bus_avg_stars, bus_review_count, bus_is_open, num_attrs, num_categories = bus_dict.get(
+        bus, (3.5, 0, None, None, None)
+    )
+    likes, upvotes = tip_dict.get((usr, bus), (0, 0))
+    num_cat, num_img = img_dict.get(bus, (0, 0))
+
+    return (
+        (usr, bus),
+        [
+            useful,
+            funny,
+            cool,
+            usr_review_count,
+            usr_useful,
+            usr_funny,
+            usr_cool,
+            usr_fans,
+            usr_avg_stars,
+            num_elite,
+            num_friends,
+            usr_avg_comp,
+            membership_years,
+            bus_avg_stars,
+            bus_review_count,
+            bus_is_open,
+            num_attrs,
+            num_categories,
+            likes,
+            upvotes,
+            num_cat,
+            num_img,
+        ],
+        float(rating),
+    )
+
+
+def save_data(data: list, output_file_name: str):
+    header = ["user_id", "business_id", "prediction"]
+    with open(output_file_name, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
+
+
+def task(folder_path, test_file_name, output_file_name):
+    start_time = time.time()
+
+    # Initialize Spark
+    conf = SparkConf().setAppName("Competition: Recommendation system")
+    spark = SparkContext(conf=conf).getOrCreate()
+    spark.setLogLevel("ERROR")
+
+    try:
+        data_reader = DataReader(spark, folder_path)
+
+        # Additional Data
+        usr_rdd = data_reader.read_json_spark("user.json")
+        usr_rdd = UserData.process(usr_rdd)
+        usr_rdd = usr_rdd.cache().collectAsMap()
+
+        bus_rdd = data_reader.read_json_spark("business.json")
+        bus_rdd = BusinessData.process(bus_rdd)
+        bus_rdd = bus_rdd.cache().collectAsMap()
+
+        review_rdd = data_reader.read_json_spark("review_train.json")
+        review_rdd = ReviewData.process(review_rdd)
+        review_rdd = review_rdd.cache().collectAsMap()
+
+        tip_rdd = data_reader.read_json_spark("tip.json")
+        tip_rdd = TipData.process(tip_rdd)
+        tip_rdd = tip_rdd.cache().collectAsMap()
+
+        img_rdd = data_reader.read_json_spark("photo.json")
+        img_rdd = PhotoData.process(img_rdd)
+        img_rdd = img_rdd.cache().collectAsMap()
+
+        # cin_rdd = data_reader.read_json_spark("checkin.json")
+
+        train_rdd, _ = data_reader.read_csv_spark("yelp_train.csv")
+
+        # Read validation dataset
+        test_file_name = os.path.basename(test_file_name)
+        val_rdd, _ = data_reader.read_csv_spark(test_file_name)
+
+        # Merge datasets
+        train_rdd = train_rdd.map(lambda row: create_dataset(row, usr_rdd, bus_rdd, review_rdd, tip_rdd, img_rdd))
+        val_processed = val_rdd.map(lambda row: create_dataset(row, usr_rdd, bus_rdd, review_rdd, tip_rdd, img_rdd))
+
+        # Extract X_train and Y_train
+        X_train = train_rdd.map(lambda x: x[1]).cache()
+        X_train = np.array(X_train.collect(), dtype="float32")
+        Y_train = train_rdd.map(lambda x: x[2]).cache()
+        Y_train = np.array(Y_train.collect(), dtype="float32")
+
+        # Extract X_train and Y_train
+        X_val = val_processed.map(lambda x: x[1]).cache()
+        X_val = np.array(X_val.collect(), dtype="float32")
+
+        xgb = XGBRegressor()
+        xgb.fit(X_train, Y_train)
+        Y_pred = xgb.predict(X_val)
+
+        pred_data = []
+        for i, row in enumerate(val_rdd.collect()):
+            pred_data.append([row[0], row[0], Y_pred[i]])
+
+        save_data(pred_data, output_file_name)
+
+    except Exception as e:
+        print(f"Exception occured:\n{e}")
+
+    finally:
+        spark.stop()
+
+    execution_time = time.time() - start_time
+    print(f"Duration: {execution_time}\n")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: spark-submit competition.py <folder_path> <test_file_name> <output_file_name>")
+        sys.exit(1)
+
+    # Read input parameters
+    folder_path = sys.argv[1]
+    test_file_name = sys.argv[2]
+    output_file_name = sys.argv[3]
+
+    task(folder_path, test_file_name, output_file_name)
---
+++ competition/solution/old.py
@@ -0,0 +1,292 @@
+import csv
+import json
+import sys
+import time
+
+import numpy as np
+from pyspark import SparkConf, SparkContext
+from xgboost import XGBRegressor
+
+
+def save_data(data, output_file_name):
+    header = ["user_id", "business_id", "prediction"]
+    with open(output_file_name, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
+
+
+class ItemBasedCF:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def prepare_dataset(data, split="train"):
+        # Remove the header
+        header = data.first()
+        data = (
+            data.filter(lambda row: row != header)
+            .map(lambda row: row.split(","))
+            .map(lambda row: (row[0], row[1], row[2]) if split == "train" else (row[0], row[1]))
+        )
+        return data
+
+    @staticmethod
+    def get_bus_to_usr_map(train_data):
+        # Group by business_id and collect the corresponding set of users
+        bus2user = (
+            train_data.map(lambda x: (x[1], (x[0], float(x[2]))))
+            .groupByKey()
+            .mapValues(lambda vals: {"users": dict(vals), "avg_rating": sum(val[1] for val in vals) / len(vals)})
+        )
+        return bus2user.collectAsMap()
+
+    @staticmethod
+    def get_usr_to_bus_map(train_data):
+        # Group by user_id and collect the corresponding set of businesses
+        user2bus = (
+            train_data.map(lambda x: (x[0], (x[1], float(x[2]))))
+            .groupByKey()
+            .mapValues(lambda vals: {"business": dict(vals)})
+        )
+        return user2bus.collectAsMap()
+
+    @staticmethod
+    def compute_pearson_similarity(data, item2user_dict):
+        """
+        Formala: r = Σᵢ((xᵢ − mean(x))(yᵢ − mean(y))) (√Σᵢ(xᵢ − mean(x))² √Σᵢ(yᵢ − mean(y))²)⁻¹
+        """
+        # Unpack the data
+        item1, item2 = data
+
+        # Find common user to calculate co-rated averages
+        users_item1 = set(item2user_dict[item1]["users"].keys())
+        users_item2 = set(item2user_dict[item2]["users"].keys())
+        common_users = users_item1.intersection(users_item2)
+
+        if len(common_users) <= 1:
+            similarity = (5 - abs(item2user_dict[item1]["avg_rating"] - item2user_dict[item2]["avg_rating"])) / 5
+        else:
+            r1 = []
+            r2 = []
+            # Get ratings of common users for both business
+            for usr in common_users:
+                r1.append(item2user_dict[item1]["users"][usr])
+                r2.append(item2user_dict[item2]["users"][usr])
+
+            # Center the ratings by subtracting the co-rated average rating
+            r1_bar = sum(r1) / len(r1)
+            r2_bar = sum(r2) / len(r2)
+            r1 = [r - r1_bar for r in r1]
+            r2 = [r - r2_bar for r in r2]
+
+            # Compute weight for the item pair
+            numer = sum([a * b for a, b in zip(r1, r2)])
+            denom = ((sum([a**2 for a in r1])) ** 0.5) * (sum([b**2 for b in r2]) ** 0.5)
+
+            similarity = 0 if denom == 0 else numer / denom
+
+        return similarity
+
+    @staticmethod
+    def predict_rating(data, bus2user_dict, user2bus_dict, neighbours=15):
+        """Perform Item-based Collaborative filtering on prepared data."""
+        # Unpack the data
+        user, business = data
+
+        # Return avg rating if user or business is not present in the dataset
+        if user not in user2bus_dict or business not in bus2user_dict:
+            return 3.0
+
+        # Pearson similarities for rating prediction
+        pc = []
+
+        for item in user2bus_dict[user]["business"].keys():
+            # Compute pearson similarity for each business pair
+            similarity = ItemBasedCF.compute_pearson_similarity((business, item), bus2user_dict)
+            pc.append((similarity, bus2user_dict[item]["users"][user]))
+
+        # Calculate the predicted rating
+        top_pc = sorted(pc, key=lambda x: -x[0])[:neighbours]
+        x, y = 0, 0
+        for p, r in top_pc:
+            x += p * r
+            y += abs(p)
+        predicted_rating = 3.5 if y == 0 else x / y
+
+        return predicted_rating
+
+    def run(self, spark, train_file_name, test_file_name):
+        # Read and process the train data
+        train_data = spark.textFile(train_file_name)
+        train_data = ItemBasedCF.prepare_dataset(train_data, split="train")
+
+        # Preprocess train data to get mapping dictionaries
+        bus2user_dict = ItemBasedCF.get_bus_to_usr_map(train_data)
+        user2bus_dict = ItemBasedCF.get_usr_to_bus_map(train_data)
+
+        # Read and prepare validation data
+        val_data = spark.textFile(test_file_name)
+        val_data = ItemBasedCF.prepare_dataset(val_data, split="valid").cache()
+
+        val_data = val_data.map(
+            lambda x: [x[0], x[1], ItemBasedCF.predict_rating(x, bus2user_dict, user2bus_dict)]
+        ).cache()
+
+        return val_data
+
+
+class ModelBased:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def read_csv_spark(path, sc):
+        rdd = sc.textFile(path)
+        header = rdd.first()
+        rdd = rdd.filter(lambda row: row != header).map(lambda row: row.split(","))
+        return rdd
+
+    @staticmethod
+    def read_json_spark(path, sc):
+        return sc.textFile(path).map(lambda row: json.loads(row))
+
+    @staticmethod
+    def process_reviews(review_rdd):
+        review_rdd = (
+            review_rdd.map(
+                lambda row: (row["business_id"], (float(row["useful"]), float(row["funny"]), float(row["cool"])))
+            )
+            .groupByKey()
+            .mapValues(lambda x: tuple(sum(col) / len(col) for col in zip(*x)))
+            .cache()
+        )
+        return review_rdd.collectAsMap()
+
+    @staticmethod
+    def process_user(usr_rdd):
+        usr_rdd = usr_rdd.map(
+            lambda row: (row["user_id"], (float(row["average_stars"]), float(row["review_count"]), float(row["fans"])))
+        ).cache()
+        return usr_rdd.collectAsMap()
+
+    @staticmethod
+    def process_bus(bus_rdd):
+        bus_rdd = bus_rdd.map(
+            lambda row: (row["business_id"], (float(row["stars"]), float(row["review_count"])))
+        ).cache()
+        return bus_rdd.collectAsMap()
+
+    @staticmethod
+    def process_train_data(row, review_dict, usr_dict, bus_dict):
+        if len(row) == 3:
+            usr, bus, rating = row
+        else:
+            usr, bus = row
+            rating = None
+
+        useful, funny, cool = review_dict.get(bus, (None, None, None))
+        usr_avg_star, usr_review_cnt, usr_fans = usr_dict.get(usr, (None, None, None))
+        bus_avg_star, bus_review_cnt = bus_dict.get(bus, (None, None))
+
+        return ([useful, funny, cool, usr_avg_star, usr_review_cnt, usr_fans, bus_avg_star, bus_review_cnt], rating)
+
+    def run(self, spark, folder_path, test_file_name):
+        # Read and process the train data
+        train_rdd = ModelBased.read_csv_spark(folder_path + "/yelp_train.csv", spark)
+
+        review_rdd = ModelBased.read_json_spark(folder_path + "/review_train.json", spark)
+        review_rdd = ModelBased.process_reviews(review_rdd)
+
+        usr_rdd = ModelBased.read_json_spark(folder_path + "/user.json", spark)
+        usr_rdd = ModelBased.process_user(usr_rdd)
+
+        bus_rdd = ModelBased.read_json_spark(folder_path + "/business.json", spark)
+        bus_rdd = ModelBased.process_bus(bus_rdd)
+
+        # Read and process validation dataset
+        val_rdd = ModelBased.read_csv_spark(test_file_name, spark).cache()
+
+        # Train X and Y
+        train_rdd = train_rdd.map(lambda x: ModelBased.process_train_data(x, review_rdd, usr_rdd, bus_rdd))
+
+        # Valid x and Y
+        val_processed = val_rdd.map(lambda x: ModelBased.process_train_data(x, review_rdd, usr_rdd, bus_rdd))
+
+        # Extract X_train and Y_train
+        X_train = train_rdd.map(lambda x: x[0]).cache()
+        X_train = np.array(X_train.collect(), dtype="float32")
+        Y_train = train_rdd.map(lambda x: x[1]).cache()
+        Y_train = np.array(Y_train.collect(), dtype="float32")
+
+        # Extract X_train and Y_train
+        X_val = val_processed.map(lambda x: x[0]).cache()
+        X_val = np.array(X_val.collect(), dtype="float32")
+
+        xgb = XGBRegressor()
+        xgb.fit(X_train, Y_train)
+        Y_pred = xgb.predict(X_val)
+
+        pred_data = []
+        for i, row in enumerate(val_rdd.collect()):
+            pred_data.append([row[0], row[1], Y_pred[i]])
+
+        return spark.parallelize(pred_data)
+
+
+def hybrid_pred(preds, factor=0.5):
+    wieghted_pred = factor * preds[0] + (1 - factor) * preds[1]
+    return wieghted_pred
+
+
+def task2_3(folder_path, test_file_name, output_file_name):
+    # Initialize Spark
+    conf = SparkConf().setAppName("Task 2.3: Hybrid recommendation system")
+    spark = SparkContext(conf=conf).getOrCreate()
+    spark.setLogLevel("ERROR")
+
+    try:
+        start_time = time.time()
+
+        # Train the item-based collaborative recommendation system
+        item_based = ItemBasedCF()
+        item_based_pred = item_based.run(
+            spark=spark, train_file_name=f"{folder_path}/yelp_train.csv", test_file_name=test_file_name
+        )
+        item_based_pred = item_based_pred.map(lambda x: ((x[0], x[1]), x[2])).persist()
+
+        # Train the item-based collaborative recommendation system
+        model_based = ModelBased()
+        model_based_pred = model_based.run(spark=spark, folder_path=folder_path, test_file_name=test_file_name)
+        model_based_pred = model_based_pred.map(lambda x: ((x[0], x[1]), x[2])).persist()
+
+        FACTOR = 0.05222
+
+        joined_preds = (
+            item_based_pred.join(model_based_pred)
+            .map(lambda x: [x[0][0], x[0][1], hybrid_pred(x[1], factor=FACTOR)])
+            .cache()
+        )
+
+        save_data(joined_preds.collect(), output_file_name)
+
+        execution_time = time.time() - start_time
+        print(f"Duration: {execution_time}\n")
+
+    finally:
+        spark.stop()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: spark-submit task2_1.py <folder_path> <test_file_name> <output_file_name>")
+        sys.exit(1)
+
+    # Read input parameters
+    folder_path = sys.argv[1]
+    test_file_name = sys.argv[2]
+    output_file_name = sys.argv[3]
+
+    task2_3(folder_path, test_file_name, output_file_name)
+
+# task2_3("HW3StudentData", "HW3StudentData/yelp_val.csv", "t2_3.csv")
